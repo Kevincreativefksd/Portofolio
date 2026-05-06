235bfca9b767d87fa873323a2f7cb3c8bfa5152f
Kevincreativefksd <fakhsyadmuhammad@gmail.com>
2026-05-06 18:13:33 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import hero from "@/components/hero";
-import problem from "@/components/problem";
-import solution from "@/components/solution";
-import portfolio from "@/components/portfolio";
-import services from "@/components/services";
-import cta from "@/components/cta";
-
-export const metadata = {
-  title: "SEO Architect untuk UMKM | Jasa SEO Indonesia",
-  description:
-    "Saya membantu UMKM meningkatkan traffic organik dan penjualan dengan strategi SEO berbasis data.",
-};
-
 export default function Home() {
-  return (
-    <main className="bg-white text-black dark:bg-black dark:text-white">
-      <hero />
-      <problem />
-      <solution />
-      <portfolio />
-      <services />
-      <cta />
-    </main>
-  );
+  return <h1>WORKING</h1>;
 }
